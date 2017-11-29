cbcb77d3decfd130da34bbd17e7db73722685b89
ziyang.wen <wenziyangday@gmail.com>
2017-11-29 14:05:08 +0800

    后台首页主结构部分

---
+++ src/components/admin/Incontainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="in-container">
+    2121212112
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'in-container'
+  }
+</script>
+
+<style>
+  .in-container {
+    padding-left: 20px;
+  }
+</style>
---
+++ src/components/admin/sidebar.vue
@@ -0,0 +1,71 @@
+<template>
+  <el-row class="tac sidebar">
+    <h1 class="logo">logo</h1>
+    <el-col :span="24">
+      <el-menu default-active="2" @open="handleOpen" @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    name: 'sidebar',
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+  .sidebar {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 200px;
+    height: 100%;
+    z-index: 0;
+    border-right: solid 1px #e6e6e6;
+  }
+  
+  .logo {
+    font-size: 18px;
+    text-align: center;
+    line-height: 56px;
+  }
+  
+  .el-menu {
+    border: 0;
+  }
+</style>
