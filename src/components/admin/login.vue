3d8c016a6cdde70737a8c98c63c6f9e8f9fb2469
ziyang.wen <wenziyangday@gmail.com>
2017-12-11 17:48:17 +0800

    添加背景图片

--- src/components/admin/login.vue
+++ src/components/admin/login.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="login">
-    <h2>XXXX</h2>
-    {{user}}
-    <button @click="login()">jkjas</button>
+    <div class="in-login">
+      <h2>XXXX</h2>
+      {{user}}
+      <button>jkjas</button>
+    </div>
+
   </div>
 </template>
 
 <script>
-  import { mapState, mapMutations, mapActions } from 'vuex'
+  import {mapState, mapMutations, mapActions} from 'vuex'
+
   export default {
     name: 'login',
     computed: {
@@ -17,5 +21,22 @@
 </script>
 
 <style>
+  .login {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: flex-end;
+    background: url(../../assets/login.jpg) center no-repeat;
+    background-size: cover;
+    overflow: hidden;
+  }
 
+  .in-login {
+    flex: 0 0 300px;
+    width: 300px;
+    min-height: 400px;
+    margin-right: 150px;
+    background: rgba(255, 255, 255, 0.77);
+  }
 </style>
