46cfb52d3fa2d6cec40790117f473ca3af84f9e2
ziyang.wen <wenziyangday@gmail.com>
2018-02-07 15:48:21 +0800

    父子组件之间的数据传输以及同级组件之间的数据

--- src/views/indexScroll/indexScroll.vue
+++ src/views/indexScroll/indexScroll.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="indexScroll">
-    <LeftIS></LeftIS>
-    <RightIS></RightIS>
+    <LeftIS :LIData="LIData"></LeftIS>
+    <RightIS :rIData="rIData" @leftTitle="leftTitles"></RightIS>
   </div>
 </template>
 
@@ -11,8 +11,52 @@
   
   export default {
     name: "indexScroll",
+    data() {
+      return {
+        LIData: [],
+        rIData: {
+          title: '标题',
+          content: `
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #9d1dce; color: #ffffff; font-size: 50px;">标题1</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #c2ccd1; color: #ffffff; font-size: 50px;">标题2</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #cea667; color: #ffffff; font-size: 50px;">标题3</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #896cce; color: #ffffff; font-size: 50px;">标题4</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #ce3f1d; color: #ffffff; font-size: 50px;">标题5</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #74ce54; color: #ffffff; font-size: 50px;">标题6</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #5193ce; color: #ffffff; font-size: 50px;">标题7</h1>
+            <h1 style="width: 100%; height: 200px; display: flex; align-items: center; justify-content: center; background: #000000; color: #ffffff; font-size: 50px;">标题8</h1>
+          `,
+          info: [
+            {name: '作者', value: '张三'},
+            {name: '创建时间', value: '2018-02-07'},
+            {name: '来源', value: '江浙日报'},
+            {name: '阅读次数', value: 152},
+          ],
+          supFun: {}
+        }
+      }
+    },
     components: {
       LeftIS, RightIS
+    },
+    methods: {
+      handleScroll() {
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+        let tags = document.getElementsByTagName('h1');
+        
+        for (let itemTag of tags) {
+          this.LIData.push(itemTag.innerHTML);
+        }
+        
+        console.log(scrollTop, tags.length, typeof tags);
+      },
+      leftTitles: function (titleData) {
+        this.LIData = titleData;
+      }
+    },
+    mounted() {
+      // window.addEventListener('scroll', this.handleScroll);
+      // this.leftTitles();
     }
   }
 </script>
--- src/views/indexScroll/leftIS.vue
+++ src/views/indexScroll/leftIS.vue
@@ -1,12 +1,29 @@
 <template>
   <div class="leftIS">
-    leftIS
+    <dl>
+      <dt>需求整理</dt>
+      <dd>1.自定义配置这个模块是否拥有</dd>
+      <dd>2.标题的数据</dd>
+      <dd>3.模块滚到一定的位置滑到底部位置</dd>
+    </dl>
+    <ul>
+      <li v-for="(liItem, index) of LIData">{{index+1}}、{{liItem}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
   export default {
-    name: "leftIS"
+    name: "leftIS",
+    data() {
+      return {
+        LISData: {
+          isHave: true,
+          data: [],
+        }
+      }
+    },
+    props: ['LIData']
   }
 </script>
 
@@ -15,7 +32,10 @@
     position: fixed;
     right: 20px;
     width: 150px;
-    height: 150px;
-    background: #00a0e9;
+    min-height: 150px;
+  }
+  li {
+    line-height: 30px;
+    font-size: 16px;
   }
 </style>
